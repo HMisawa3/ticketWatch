bce68da385db38c0c1277b1b0003741617447e59
Mintia <mintia.jaxa@gmail.com>
2023-12-03 01:30:15 +0900

    処理の記述をアレンジ

--- ticketWatch/spiders/latest_games.py
+++ ticketWatch/spiders/latest_games.py
@@ -7,10 +7,19 @@ class LatestGamesSpider(scrapy.Spider):
     start_urls = ["https://store.steampowered.com/?l=japanese"]
 
     def parse(self, response):
-        for i in range(1, 12):
+        for i, game in enumerate(response.xpath('//*[@id="tab_newreleases_content"]/a')):
             yield {
-                'Title': response.xpath('//*[@id="tab_newreleases_content"]/a[' + str(i) + ']/div[3]/div[1]/text()').get(),
-                'URL': response.xpath('//*[@id="tab_newreleases_content"]/a[' + str(i) + ']/@href').get()
+                'Title': game.xpath('.//div[3]/div[1]/text()').get(),
+                'URL': game.xpath('.//@href').get()
             }
+            if i == 10:
+                break
+
+        # 初期記述
+        # for i in range(1, 12):
+        #     yield {
+        #         'Title': response.xpath('//*[@id="tab_newreleases_content"]/a[' + str(i) + ']/div[3]/div[1]/text()').get(),
+        #         'URL': response.xpath('//*[@id="tab_newreleases_content"]/a[' + str(i) + ']/@href').get()
+        #     }
 
         pass
